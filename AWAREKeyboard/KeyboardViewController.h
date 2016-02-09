46d5bb42acc0845a4fc0a8a03811163908cf8743
tetujin <tetujin@ht.sfc.keio.ac.jp>
2016-02-09 09:06:41 -0500

    Add AWAREKeyboard

---
+++ AWAREKeyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  AWAREKeyboard
+//
+//  Created by Yuuki Nishiyama on 2/9/16.
+//  Copyright © 2016 Yuuki NISHIYAMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
