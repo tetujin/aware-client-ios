b56603989af1257d6fdb29d7240b725fa6bbd77a
Arash Badie Modiri <arashbm@gmail.com>
2016-09-15 00:05:04 +0300

    cert pinning interface

---
+++ AWARE/AWAREFramework/Core/CertPinning.m
@@ -0,0 +1,13 @@
+//
+//  CertPinning.m
+//  AWARE
+//
+//  Created by Badie Modiri Arash on 26/08/16.
+//  Copyright © 2016 Yuuki NISHIYAMA. All rights reserved.
+//
+
+#import "CertPinning.h"
+
+@implementation CertPinning
+
+@end
